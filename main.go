7416f4f8087ad3b0ceb10d63547da5c1bb9a3c56
Mads Moi-Aune <609372+madsaune@users.noreply.github.com>
2022-03-09 10:56:14 +0100

    add two new titles and refactor code

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 var (
 	vocals       = []string{"a", "e", "i", "o", "u"}
 	consonants   = []string{"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"}
-	titles       = []string{"Lord", "Dark", "Mistress", "King", "Queen", "Lady", "Mister"}
+	titles       = []string{"Lord", "Dark", "Mistress", "King", "Queen", "Lady", "Mister", "Little", "Big"}
 	worlds       = []string{"Adra", "Alumbra", "Antica", "Ardera", "Astera", "Bastia", "Batabra", "Belobra", "Bona", "Cadebra", "Calmera", "Celebra", "Celesta", "Collabra", "Damora", "Descubra", "Dibra", "Epoca", "Famosa", "Fera", "Ferobra", "Firmera", "Gentebra", "Gladera", "Harmonia", "Havera", "Honbra", "Illusera", "Impulsa", "Inabra", "Kalibra", "Karna", "Libertabra", "Lobera", "Luminera", "Lutabra", "Marbera", "Marcia", "Menera", "Monza", "Mudabra", "Mykera", "Nefera", "Nossobra", "Ocebra", "Olima", "Ombra", "Optera", "Pacera", "Peloria", "Premia", "Quelibra", "Quintera", "Refugia", "Reinobra", "Seanera", "Secura", "Serdebra", "Solidera", "Suna", "Talera", "Tembra", "Thyria", "Trona", "Utobra", "Venebra", "Versa", "Visabra", "Vunira", "Wintera", "Wizera", "Xandebra", "Yonabra", "Zenobra", "Zuna", "Zunera"}
 	nameLength   int
 	includeWorld bool
@@ -26,15 +26,15 @@ func main() {
 	flag.BoolVar(&includeTitle, "include-title", false, "include title in name")
 	flag.Parse()
 
-	fmt.Println(createRandomName(nameLength, includeWorld, includeTitle))
+	fmt.Println(createRandomName(nameLength, includeTitle, includeWorld))
 }
 
 func getRandomInt(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
-func createRandomName(length int, ofWorld, title bool) string {
-
+func createRandomName(length int, includeTitle, includeWorld bool) string {
+	// Create a nickname consiting of consonants+vocals+consonants+vocals etc..
 	var chars []string
 	for i := 0; i < length; i += 1 {
 		if i%2 != 0 {
@@ -44,16 +44,13 @@ func createRandomName(length int, ofWorld, title bool) string {
 		}
 	}
 
-	name := strings.Join(chars, "")
-	name = strings.Title(name)
+	name := strings.Title(strings.Join(chars, ""))
 
-	// If has title
-	if title {
+	if includeTitle {
 		name = fmt.Sprintf("%s %s", titles[getRandomInt(0, len(titles)-1)], name)
 	}
 
-	// If has "of world" at the end
-	if ofWorld {
+	if includeWorld {
 		name = fmt.Sprintf("%s of %s", name, worlds[getRandomInt(0, len(worlds)-1)])
 	}
 
